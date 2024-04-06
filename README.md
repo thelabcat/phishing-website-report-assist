207f059cd9c1a396b8138ab5d43a37db9a1b40e6
Wilbur Jaywright <zargulthewizard@outlook.com>
2024-04-06 17:37:06 -0400

    Config.toml
    - Separated log and config files
    - Removed double email function
    - Moved site list to config file

--- README.md
+++ README.md
@@ -5,21 +5,18 @@ A tool to help with reporting phishing websites to the domain seller and several
 This tool was designed to go with, and has a list of websites from, this video by ThioJoe on YouTube https://www.youtube.com/watch?v=0fIUiv9-UFk
 Hope it helps! TYJ, I learned how to do proper tkinter resizing while writing this :-D
 
-# What it does
+## What it does
 The program is simple: Open a browser to several online security service report pages, and give the user an easy way to copy-paste the same report information to each of them. It has buttons to read the user's name and email from a config file and copy them to the clipboard (required for some reports), an entry field for the phishing URL itself with a copy button, and a large text field for a description of the malicious site also with a copy button.
 
-
-
-This program expects firefox to be in PATH in the command to open the browser (will be the case if you are using system-package firefox on Linux), but you can edit this. I intend to incorporate strong Windows compatibility soon with webbrowser or another library, but right now it's helpful that the sites are loaded in order.
-
-Change info in the config/log file as desired.
+This program expects firefox to be in PATH, for the command to open the browser (will be the case if you are using system-package firefox on Linux), but you can edit the path to it in the config. I intend to incorporate strong Windows compatibility soon with webbrowser or another library, but right now it's helpful that the sites are loaded in order.
 
 # How to use
-1. Have the url of the phishing site on the clipboard, and save a screenshot of the site itself, showing the most self-incriminating part. WARNING: Some malicious sites have been known to be very agressive. Do not open a link you believe could be an advanced cyberattack, even in a private browsing window with a VPN to hide your location.
-2. Run this program. Firefox will open with tabs to various reporting websites (plus a whois lookup so you can contact the host company and report the site there).
-3. Paste the phishing site url in my program's field for such, and type out a description of what the site seems to be doing, along eith any details that might help convict it.
-4. If your desktop supports window tiling, I like to tile the my program on the left side of the screen, and Firefox on the right. You can move the halfway point between windows after tiling in most desktops.
-5. Go through each Firefox tab, and follow the steps to report (or lookup then report) the phishing site to each company.
-6. Check your email. Currently, TrendMicro requires email confirmation before they will review the report. Also, you will receive emails from the companies notifying you of the report status. Some companies may require resubmission before a human will review the site. You can check my program's log for the description you previously wrote for the site.
+1. Configure your name and email that you wish to use in the TOML configuration file, and rename it from `config-TEMPLATE.toml` to `config.toml`.
+2. Have the url of the phishing site on the clipboard, and save a screenshot of the site itself, showing the most self-incriminating part. WARNING: Some malicious sites have been known to be very agressive. Do not open a link you believe could be an advanced cyberattack, even in a private browsing window with a VPN to hide your location.
+3. Run this program. A browser will open with tabs to various reporting websites (plus a whois lookup so you can contact the host company and report the site there).
+4. Paste the phishing site url in my program's field for such, and type out a description of what the site seems to be doing, along with any details that might help convict it.
+5. If your desktop supports window tiling, I like to tile the my program on the left side of the screen, and Firefox on the right. You can move the halfway point between windows after tiling in most desktops.
+6. Go through each browser tab, and follow the steps to report (or lookup then report) the phishing site to each company.
+7. Check your email. Currently, TrendMicro requires email confirmation before they will review the report. Also, you will receive emails from the companies notifying you of the report status. Some companies may require resubmission before a human will review the site. You can check my program's phishing_reports.txt log for the description you previously wrote for the site.
 
 S.D.G.
--- phishing_site_report_assist.py
+++ phishing_site_report_assist.py
@@ -1,58 +1,26 @@
 #!/usr/bin/env python3
-#Fraud website reporting assistant v1.0
+#Fraud website reporting assistant
 #S.D.G.
 
 #import all modules
 from tkinter import *
 import tkinter.messagebox as mb
 import os #To launch browser
-
-REPORT_FILE="./phishing_reports.txt" #config and logs are stored here
+import tomllib
 
 #Load config
+CONFIG_FILE = "config.toml"
 try:
-    f=open(REPORT_FILE)
-except FileNotFoundError:
-    mb.showerror(title="No congif/records file", message=REPORT_FILE+" does not exist. Please create with config.")
-    quit()
-
-try:
-    NAME = f.readline().strip()
-    SHIELD_EMAIL = f.readline().strip()
-    ACTUAL_EMAIL = f.readline().strip()
-    EMAILS = (SHIELD_EMAIL, ACTUAL_EMAIL)
+    f = open(CONFIG_FILE)
+    CONFIG = tomllib.loads(f.read())
     f.close()
-    if not ("@" in SHIELD_EMAIL and "@" in ACTUAL_EMAIL): #rudimentary checks that the config data is actually config data
-        raise EOFError
-
-except EOFError:
-    mb.showerror(title="Incomplete congif/records file", message=REPORT_FILE+" did not contain config. Please create with config.")
+except FileNotFoundError:
+    mb.showerror("No config file", "Could not find " + CONFIG_FILE)
     quit()
-
-
-SECURITY_SITES = ( #List of sites to open
-"https://digital.com/best-web-hosting/who-is/",
-"https://safebrowsing.google.com/safebrowsing/report_phish/",
-"https://www.microsoft.com/en-us/wdsi/support/report-unsafe-site",
-"https://www.fortiguard.com/webfilter",
-"https://www.brightcloud.com/tools/url-ip-lookup.php",
-"https://threatcenter.crdf.fr/submit_url.html",
-"https://report.netcraft.com/report",
-"https://urlfiltering.paloaltonetworks.com/",
-"https://phishing.eset.com/en-us/report",
-"https://global.sitesafety.trendmicro.com/index.php",
-"https://www.bitdefender.com/consumer/support/answer/29358/",
-"https://sitelookup.mcafee.com/",
-"https://csi.forcepoint.com/",
-"https://sitereview.symantec.com/#/",
-"https://www.spam404.com/report.html",
-"https://opentip.kaspersky.com/",
-"https://talosintelligence.com/reputation_center"
-)
 
 def open_browser():
     """Command to launch browser with all security sites in separate tabs"""
-    os.popen("firefox " + " ".join(SECURITY_SITES)) #Command to launch browser with all security sites in different tabs
+    os.popen(CONFIG["browserPath"] + " " + " ".join(CONFIG["securitySites"])) #Command to launch browser with all security sites in different tabs
 
 class Window(Tk):
     def __init__(self):
@@ -62,8 +30,8 @@ class Window(Tk):
         self.title("Phishing Website Report Assistant")
 
         #Site URL and comment, used for log.
-        self.reported_url=""
-        self.report_comment=""
+        self.reported_url = ""
+        self.report_comment = ""
 
         self.build() #construct GUI
         self.mainloop() #start GUI
@@ -72,50 +40,49 @@ class Window(Tk):
         """Construct the GUI"""
 
         #Buttons for copying your ID info
-        Button(self, text="Copy name", command=self.copy_name).grid(row=0, sticky=E+W)
-        Button(self, text="Copy shielded email", command=lambda: self.copy_email(0)).grid(row=1, sticky=E+W)
-        Button(self, text="Copy actual email", command=lambda: self.copy_email(1)).grid(row=2, sticky=E+W)
+        Button(self, text = "Copy your name", command = self.copy_name).grid(row = 0, sticky = E + W)
+        Button(self, text = "Copy your email", command = self.copy_email).grid(row = 1, sticky = E + W)
 
         #Subframe with entry field for malicious URL, and a button to copy it
         self.url_frame=Frame(self)
-        self.url_frame.grid(row=3, sticky=E+W)
+        self.url_frame.grid(row = 2, sticky=E+W)
 
-        Label(self.url_frame, text="Phishing URL:").grid(row=0, column=0, sticky=E)
+        Label(self.url_frame, text = "Phishing URL:").grid(row = 0, column = 0, sticky = E)
 
-        self.url_entry=Entry(self.url_frame)
-        self.url_entry.grid(row=0, column=1, sticky=N+S+E+W)
+        self.url_entry = Entry(self.url_frame)
+        self.url_entry.grid(row = 0, column = 1, sticky = NSEW)
 
-        Button(self.url_frame, text="Copy URL", command=self.copy_url).grid(row=0, column=2)
+        Button(self.url_frame, text = "Copy URL", command = self.copy_url).grid(row=0, column=2)
 
-        self.url_frame.columnconfigure(1, weight=1) #Set center column (the one with the field) to expand sideways)
+        self.url_frame.columnconfigure(1, weight = 1) #Set center column (the one with the field) to expand sideways)
 
         #Subframe for malicious site description comment area and a button to copy it
-        self.desc_frame=Frame(self)
-        self.desc_frame.grid(row=4, sticky=N+S+E+W)
+        self.desc_frame = Frame(self)
+        self.desc_frame.grid(row = 3, sticky=N+S+E+W)
 
-        Label(self.desc_frame, text="Phishing description:").grid(row=0, sticky=N+E+W)
+        Label(self.desc_frame, text = "Phishing description:").grid(row=0, sticky=N+E+W)
 
-        self.desc_text=Text(self.desc_frame, width=30, height=20, wrap="word")
-        self.desc_text.grid(row=1, sticky=N+S+E+W)
+        self.desc_text = Text(self.desc_frame, width = 30, height = 20, wrap = "word")
+        self.desc_text.grid(row = 1, sticky = NSEW)
 
-        Button(self.desc_frame, text="Copy description", command=self.copy_desc).grid(row=2, sticky=E+W+S)
+        Button(self.desc_frame, text = "Copy description", command = self.copy_desc).grid(row = 2, sticky = S + E + W)
 
-        self.desc_frame.rowconfigure(1, weight=1) #Set comment text area to expand vertically
-        self.desc_frame.columnconfigure(0, weight=1) #Comment frame is one column wide. Set to expand horizontally
+        self.desc_frame.rowconfigure(1, weight = 1) #Set comment text area to expand vertically
+        self.desc_frame.columnconfigure(0, weight = 1) #Comment frame is one column wide. Set to expand horizontally
 
-        self.rowconfigure(4, weight=1) #Comment frame is on row 4. Set to expand vertically.
+        self.rowconfigure(3, weight = 1) #Comment frame is on row 3. Set to expand vertically.
 
-        self.columnconfigure(0, weight=1) #Root window is one column wide. Set to expand sideways.
+        self.columnconfigure(0, weight = 1) #Root window is one column wide. Set to expand sideways.
 
     def copy_name(self):
         """Copy your name to the clipboard"""
         self.clipboard_clear()
-        self.clipboard_append(NAME)
+        self.clipboard_append(CONFIG["name"])
 
-    def copy_email(self, email_index):
+    def copy_email(self):
         """Copy one of your emails to the clipboard"""
         self.clipboard_clear()
-        self.clipboard_append(EMAILS[email_index])
+        self.clipboard_append(CONFIG["email"])
 
     def copy_url(self):
         """Copy the malicious URL to the clipboard, and write it to window memory"""
@@ -137,11 +104,11 @@ window = Window() #Call main window
 #After the window closes, try to save the URL and comment to the log.
 try:
     if window.reported_url or window.report_comment: #If any information was written then copied, save it.
-        f = open(REPORT_FILE, "a")
-        f.write("\n\n"+window.reported_url+"\n"+window.report_comment)
+        f = open(CONFIG["logFile"], "a")
+        f.write(window.reported_url + "\n" + window.report_comment + "\n\n")
         f.close()
 except Exception as ex: #A save error occured. Notify the user.
-    text=str(ex)
+    text = str(ex)
     if hasattr(ex, "message"):
-        text+=" "+ex.message
-    mb.showerror(title="Save error", message=text)
+        text += " " + ex.message
+    mb.showerror(title = "Save error", message = text)
